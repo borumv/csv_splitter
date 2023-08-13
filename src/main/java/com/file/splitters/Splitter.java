3d095bbe6fbb1fbbe0931fd6660e443c9d028ec4
Falltroy <fallTroy@protonmail.com>
2023-08-13 12:35:14 +0300

    final refactoring

--- src/main/java/com/file/splitters/Splitter.java
+++ src/main/java/com/file/splitters/Splitter.java
@@ -1,10 +1,15 @@
 package com.file.splitters;
 
+import com.file.dto.IndexTuple;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Map;
 
 public interface Splitter {
     void split (Path targetToSplit, Path pathSplitFiles, int countParts) throws FileNotFoundException;
     void generateIndexes(Path sourceFile, Path targetFile, int size_buffer) throws IOException;
+
+    Map<Integer, IndexTuple> generateIndexMap(Path sourceFile, int size_buffer);
 }
